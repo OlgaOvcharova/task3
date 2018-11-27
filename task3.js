944a24c8587a5a56e603f08ae8a42eb50e3d7ce1
Olga Ovcharova <olga.ovcharova@daxx.com>
2018-11-27 14:39:55 +0200

    second edition

--- task3.js
+++ task3.js
@@ -5,18 +5,21 @@
 */
 var resultExp = 1;
 var absAcc = 0.01;
+var x = 1;
 do {
     //введите точность absAcc
     //забрать значение absAcc
 }
 while (absAcc <= 0);    
 
-for (var i=2, next=x; next > absAcc || next < -eps; i++){
+for (var i=2, next = x; next > absAcc || next < -absAcc; i++){
     resultExp = resultExp + next;
     next = next * x/i;
 }
 //вывести результат экспоненты resultExp;
-//погрешность next;
+//погрешность next; 
 //длинну членов ряда i;
 
 console.info("result: " + resultExp);
+console.info("infelicity: " + next);
+console.info("length of members row: " + i);
