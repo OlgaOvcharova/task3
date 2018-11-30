64d0e31d6de32af93f0bd145f6fc0e5def38dfdd
Olga Ovcharova <olga.ovcharova@daxx.com>
2018-11-30 17:35:58 +0200

    changes task3

--- task3.js
+++ task3.js
@@ -1,26 +1,11 @@
-/* абсолютная точность absolute accuracy - absAcc,
-результат экспоненты - resultExp, номер члена ряда - i,
-следующий член ряда - next, заданное значение x,
-
-*/
 var resultExp = 1;
-var absAcc = 0.01;
+var absAcc = document.getElementById('absAcc').value;
 var x = 1;
-do {
-    //введите точность absAcc
-    //забрать значение absAcc
 
+function myCalc() {
+    for (var i = 2, next = x; next > absAcc || next < -absAcc; i++) {
+        resultExp = resultExp + next;
+        next = next * x / i;   
+    }
+    document.getElementById("res1").innerHTML = "Результат экспоненты: " + resultExp;
 }
-while (absAcc <= 0);    
-
-for (var i=2, next = x; next > absAcc || next < -absAcc; i++){
-    resultExp = resultExp + next;
-    next = next * x/i;
-}
-//вывести результат экспоненты resultExp;
-//погрешность next; 
-//длинну членов ряда i;
-
-console.info("result: " + resultExp);
-console.info("infelicity: " + next);
-console.info("length of members row: " + i);
